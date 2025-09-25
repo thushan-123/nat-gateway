f83544fbfbc49e6e398e4006be43ca7887b6a80f
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-25 14:17:56 +0530

    imple insert ip

--- src/natTranslation/ips.rs
+++ src/natTranslation/ips.rs
@@ -12,4 +12,21 @@ impl AssignIP{
             assign_ip: HashMap::new(),
         }
     }
+
+    pub fn insert_ip(&mut self, start_public_ip :&str ,subnet_mask:&str, num_of_ip: u8){
+
+        let ip: Ipv4Addr = public_ip.parse().expect(
+            "Invalid ip address"
+        );
+
+        let mut ip_u32 = u32::form(ip);
+
+        for i in 1..num_of_ip {
+            let mut new_ip = Ipv4Addr::from(ip_u32 + i as u32);
+            self.assign_ip.insert(new_ip, false);
+        }
+
+
+
+    }
 }
