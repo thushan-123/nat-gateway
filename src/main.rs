12ae1f041a159d9dd1306cae38bc62d4fb508c5a
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-25 10:57:08 +0530

    read the tcp stream data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{Read,Write,BufRead};
+use std::net::{TcpStream, TcpListener};
+
+fn handle_client(mut stream : TcpStream){
+    let mut buffer = [0;4096];
+    
+    stream.read(&mut buffer).expect(
+        "fail to read data from client"
+    );
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
