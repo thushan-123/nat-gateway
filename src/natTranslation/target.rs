05e71a121370d68f4be95457c530b6cdbc7b9394
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-25 16:14:45 +0530

    clint data send to dest revie res

--- src/natTranslation/target.rs
+++ src/natTranslation/target.rs
@@ -1,17 +1,41 @@
 
-use std::net::{TcpStream, Ipv4Addr, IpAddr, SocketAddr};
+use std::net::{TcpStream, Ipv4Addr, IpAddr, SocketAddr, ToSocketAddrs};
 
 pub fn connect_to_target_with_kernal(
     public_ip: Ipv4Addr,
-    public_port:u16
-) -> Result<()>{
-    let translate_source_ip = IpAddr::V4(public_ip);
-    let translate_source_port = public_port;
+    public_port:u16,
+    target: &str,
+    request_data: String
+) {
+    let source_addr = SocketAddr::new(IpAddr::V4(public_ip), public_port);
 
-    let translate_source_address = SocketAddr::new(
-        translate_source_ip,
-        translate_source_port
-    );
+    // resolve target domain
+    let mut address_resolve = target.to_socket_addrs()?;
+    let target_address = address_resolve
+        .next()
+        .ok_or("No addresses resolved for target")?;
 
+    println!("Connecting from {} to {}", source_addr, target_address);
+
+    let socket = Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP))?;
+    socket.bind(&source_addr.into())?;
+
+    socket.set_read_timeout(Some(Duration::from_secs(10)))?;
+    socket.set_write_timeout(Some(Duration::from_secs(10)))?;
+
+    socket.connect(&target_address.into())?;
+
+    let mut stream: std::net::TcpStream = socket.into();
+
+    //  request data -> dest
+    stream.write_all(request_data.as_bytes())?;
+    stream.flush()?;
+
+
+    let mut buffer = Vec::new();
+    stream.read_to_end(&mut buffer)?;
+
+    let response = String::from_utf8_lossy(&buffer).to_string();
+    return response;
 
 }
